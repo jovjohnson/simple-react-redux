f20b5dec2041947da983b831202977a4c86de449
Jovvi Johnson <jjohnsow@gmail.com>
2016-10-11 17:05:03 -0700

    load courses action

--- src/actions/CourseActions.js
+++ src/actions/CourseActions.js
@@ -1,5 +1,17 @@
 import * as types from './ActionTypes'
+import courseApi from '../api/mockAuthorApi'
 
 export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course }
+  return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getAllCourses()
+    .then(courses => {
+      dispatch(loadCoursesSucces(courses));
+    }).catch(err => {
+      throw(err);
+    })
+  }
 }
