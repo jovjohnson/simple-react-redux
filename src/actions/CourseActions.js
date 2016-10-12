d5d1d804a02f48f06555473c8c99006263d9da45
Jovvi Johnson <jjohnsow@gmail.com>
2016-10-12 14:28:00 -0700

    new actions

--- src/actions/CourseActions.js
+++ src/actions/CourseActions.js
@@ -9,6 +9,15 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses }
 }
 
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course }
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course }
+}
+
+
 export function loadCourses() {
   return function(dispatch) {
     return courseApi.getAllCourses()
@@ -16,6 +25,18 @@ export function loadCourses() {
       dispatch(loadCoursesSuccess(courses));
     }).catch(err => {
       throw(err);
+    })
+  }
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return courseApi.saveCourse(course)
+    .then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+      dispatch(createCourseSuccess(savedCourse))
+    }).catch(err => {
+      throw(err)
     })
   }
 }
