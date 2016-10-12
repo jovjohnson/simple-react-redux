4927c2ad6d9ca58b1a50dcfffe2981a4ee3eba86
Jovvi Johnson <jjohnsow@gmail.com>
2016-10-11 21:28:29 -0700

    manage courses

---
+++ src/components/course/ManageCourses.js
@@ -0,0 +1,45 @@
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as courseActions from '../../actions/CourseActions'
+
+
+class ManageCoursePage extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Add Course </h2>
+        <input type="text"
+        onChange={this._onTitleChange}
+        value={this.state.course.title} />
+
+        <input className="btn btn-info" type="submit"
+        value="Save"
+        onClick={this._onClickSave}/>
+      </div>
+    )
+  }
+}
+
+ManageCoursePage.PropTypes = {
+  myProp: PropTypes.string.isRequired
+}
+
+
+function mapStateToProps(state) {
+  return {
+    state: state
+  }
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,14 @@ import App from './components/App';
 import HomePage from './components/home/HomePage'
 import AboutPage from './components/about/AboutPage'
 import CoursesPage from './components/course/CoursesPage'
+import ManageCourses from './components/course/ManageCourses'
 
 export default (
   <Route path="/" component={App} >
     <IndexRoute component={HomePage} />
     <Route path="about" component={AboutPage} />
     <Route path="courses" component={CoursesPage} />
+    <Route path="course" component={ManageCourses} />
+    <Route path="course/:id" component={ManageCourses} />
   </Route>
 );
